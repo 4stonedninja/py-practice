fc07109f43007342db732d7959016711a99f389e
Ghost <himanshujoshi8684@gmail.com>
2025-12-24 09:07:56 +0530

    Learn basic python data types

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+name = "Hemanshu"
+age = 23
+rating = 4.8
+is_learning_python = True
+
+print ("Name:", name)
+print ("Age:", age)
+print ("Rating:", rating)
+print ("Learning Python:", is_learning_python)
