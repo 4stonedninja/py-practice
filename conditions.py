0255887aafba1f69f862a05e0cedefaca939ede7
Ghost <himanshujoshi8684@gmail.com>
2025-12-25 16:05:28 +0530

    Fix typo in conditional output

--- conditions.py
+++ conditions.py
@@ -1,7 +1,7 @@
 status_code = 200
 
 if status_code == 200:
-    print ("sucess: Data received")
+    print ("success: Data received")
 elif status_code == 404:
     print ("error: Recourse not found")
 else:
