786f165966d926d1f5cae9ad66faa45c1735a1e3
Ghost <himanshujoshi8684@gmail.com>
2025-12-25 15:33:10 +0530

    Learn python lists and dictionaries

---
+++ collections.py
@@ -0,0 +1,17 @@
+# List Example
+skills = ["python", "Git", "APIs"]
+
+print("Skills:", skills)
+print("First skill:", skills[0])
+
+
+# Dictionary example
+user = {
+    "name": "Hemanshu",
+    "age": 23,
+    "skills": skills,
+    "active": True
+}
+print("user name:", user["name"])
+print("user skills:", user["skills"])
+print("Is active:", user["active"])
